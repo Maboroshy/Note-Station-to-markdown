369904717c209963ed58aa07687e83277c8d89df
Maboroshy <maboroshy@mail.ru>
2018-11-13 22:29:37 +0300

    Fixed file name safety and made some general changes

--- nsx2md.py
+++ nsx2md.py
@@ -9,6 +9,7 @@ import shutil
 import zipfile
 import tempfile
 import subprocess
+import collections
 import distutils.version
 
 from pathlib import Path
@@ -26,8 +27,25 @@ insert_mtime = False
 creation_date_in_filename = False
 force_windows_filename_limitations = False
 
+############################################################################
+
+Notebook = collections.namedtuple('Notebook', ['path', 'media_path'])
+
+
+def sanitise_path_string(path_str):
+    for char in (':', '/', '\\', '|'):
+        path_str = path_str.replace(char, '-')
+    for char in ('?', '*'):
+        path_str = path_str.replace(char, '')
+        path_str = path_str.replace('<', '(')
+        path_str = path_str.replace('>', ')')
+        path_str = path_str.replace('"', "'")
+
+    return path_str
+
 
 work_path = Path(__file__).parent
+media_dir_name = sanitise_path_string(media_dir_name)
 pandoc_input_file = tempfile.NamedTemporaryFile(delete=False)
 pandoc_output_file = tempfile.NamedTemporaryFile(delete=False)
 
@@ -55,139 +73,141 @@ else:
 
 
 if len(sys.argv) > 1:
-    files = [Path(path) for path in sys.argv[1:]]
+    files_to_convert = [Path(path) for path in sys.argv[1:]]
 else:
-    files = Path(work_path).glob('*.nsx')
+    files_to_convert = Path(work_path).glob('*.nsx')
+
+if not files_to_convert:
+    print('No .nsx files found')
+    exit(1)
 
 
-for file in files:
-    nsx = zipfile.ZipFile(str(file))
-    config_data = json.loads(nsx.read('config.json').decode('utf-8'))
+for file in files_to_convert:
+    nsx_file = zipfile.ZipFile(str(file))
+    config_data = json.loads(nsx_file.read('config.json').decode('utf-8'))
 
-    for notebook in config_data['notebook']:
-        notebook_data = json.loads(nsx.read(notebook).decode('utf-8'))
+    notebook_id_to_path_index = {}
+
+    for notebook_id in config_data['notebook']:
+        notebook_data = json.loads(nsx_file.read(notebook_id).decode('utf-8'))
         notebook_title = notebook_data['title'] or 'Untitled'
-        notebook_path = file.parent / Path(notebook_title)
-
-        print('Converting notebook "{}"'.format(notebook_title))
+        notebook_path = work_path / Path(sanitise_path_string(notebook_title))
 
         n = 1
         while notebook_path.is_dir():
-            notebook_path = file.parent / Path('{}_{}'.format(notebook_title, n))
+            notebook_path = work_path / Path('{}_{}'.format(sanitise_path_string(notebook_title), n))
             n += 1
 
         notebook_media_path = Path(notebook_path / media_dir_name)
         notebook_media_path.mkdir(parents=True)
 
-        for note in config_data['note']:
-            note_data = json.loads(nsx.read(note).decode('utf-8'))
-            note_title = note_data['title']
-            note_ctime = note_data.get('ctime', '')
-            note_mtime = note_data.get('mtime', '')
-
-            if note_data['parent_id'] != notebook:
-                continue
+        notebook_id_to_path_index[notebook_id] = Notebook(notebook_path, notebook_media_path)
 
 
-            content = re.sub('<img class="[^"]*syno-notestation-image-object" src=[^>]*ref="',
-                             '<img src="', note_data.get('content', ''))
+    converted_note_count = 0
+
+    for note_id in config_data['note']:
+        note_data = json.loads(nsx_file.read(note_id).decode('utf-8'))
+        note_title = note_data['title']
+        note_ctime = note_data.get('ctime', '')
+        note_mtime = note_data.get('mtime', '')
+        parent_notebook_id = note_data['parent_id']
+        parent_notebook = notebook_id_to_path_index[parent_notebook_id]
+
+        print('Converting note "{}"'.format(note_title))
+
+        content = re.sub('<img class="[^"]*syno-notestation-image-object" src=[^>]*ref="',
+                         '<img src="', note_data.get('content', ''))
 
 
-            Path(pandoc_input_file.name).write_text(content, 'utf-8')
-            pandoc = subprocess.Popen(pandoc_args)
-            pandoc.wait(5)
-            content = Path(pandoc_output_file.name).read_text('utf-8')
+        Path(pandoc_input_file.name).write_text(content, 'utf-8')
+        pandoc = subprocess.Popen(pandoc_args)
+        pandoc.wait(5)
+        content = Path(pandoc_output_file.name).read_text('utf-8')
 
 
-            attachment_list = []
+        attachment_list = []
 
-            for attachment in note_data.get('attachment', ''):
+        for attachment_id in note_data.get('attachment', ''):
 
-                ref = note_data['attachment'][attachment].get('ref', '')
-                md5 = note_data['attachment'][attachment]['md5']
-                name = note_data['attachment'][attachment]['name']
-                source = note_data['attachment'][attachment].get('source', '')
-
-                n = 1
-                while Path(notebook_media_path / name).is_file():
-                    name_parts = name.rpartition('.')
-                    name = ''.join((name_parts[0], '_{}'.format(n), name_parts[1], name_parts[2]))
-                    n += 1
-
-                if Path(notebook_media_path / name).is_file():
-                    name = str(round(time.time())) + '-' + name
-
-                if absolute_links:
-                    link_path = link_prepend + str(notebook_media_path) + '/' + name
-                else:
-                    link_path = link_prepend + '/' + media_dir_name + '/' + name
-
-                if convert_spaces_in_links:
-                    link_path = link_path.replace(' ', '%20')
-
-                try:
-                    Path(notebook_media_path / name).write_bytes(nsx.read('file_' + md5))
-                    attachment_list.append('[{}]({})'.format(name, link_path))
-                except Exception:
-                    if source:
-                        attachment_list.append('[{}]({})'.format(name, source))
-                    else:
-                        print('  Can\'t find attachment "{}" of note "{}"'.format(name, note_title))
-                        attachment_list.append('[NOT FOUND]({})'.format(link_path))
-
-                if ref and source:
-                    content = content.replace(ref, source)
-                elif ref:
-                    content = content.replace(ref, link_path)
-
-
-            if note_data.get('tag', '') or attachment_list or insert_title \
-                    or insert_ctime or insert_mtime:
-                content = '\n' + content
-
-            if insert_mtime and note_mtime:
-                text_mtime = time.strftime('%Y-%m-%d %H:%M', time.localtime(note_mtime))
-                content = 'Modified: {}  \n{}'.format(text_mtime, content)
-            if insert_ctime and note_ctime:
-                text_ctime = time.strftime('%Y-%m-%d %H:%M', time.localtime(note_ctime))
-                content = 'Created: {}  \n{}'.format(text_ctime, content)
-            if attachment_list:
-                content = 'Attachments: {}  \n{}'.format(', '.join(attachment_list), content)
-            if note_data.get('tag', ''):
-                content = 'Tags: {}  \n{}'.format(', '.join(note_data['tag']), content)
-            if insert_title:
-                content = '{}\n{}\n{}'.format(note_title, '=' * len(note_title), content)
-
-            if creation_date_in_filename and note_ctime:
-                note_title = time.strftime('%Y-%m-%d ', time.localtime(note_ctime)) + note_title
-
-
-            md_file_name = note_title
-            for char in (':', '/', '\\', '|'):
-                md_file_name = md_file_name.replace(char, '-')
-            for char in ('?', '*'):
-                md_file_name = md_file_name.replace(char, '')
-            md_file_name = md_file_name.replace('<', '(')
-            md_file_name = md_file_name.replace('>', ')')
-            md_file_name = md_file_name.replace('"', "'")
-
-            md_file_path = Path(notebook_path / '{}.{}'.format(md_file_name, md_file_ext))
+            ref = note_data['attachment'][attachment_id].get('ref', '')
+            md5 = note_data['attachment'][attachment_id]['md5']
+            source = note_data['attachment'][attachment_id].get('source', '')
+            name = sanitise_path_string(note_data['attachment'][attachment_id]['name'])
 
             n = 1
-            while md_file_path.is_file():
-                md_file_path = Path(notebook_path / ('{}_{}.{}'.format(md_file_name, n, md_file_ext)))
+            while Path(parent_notebook.media_path / name).is_file():
+                name_parts = name.rpartition('.')
+                name = ''.join((name_parts[0], '_{}'.format(n), name_parts[1], name_parts[2]))
                 n += 1
 
-            md_file_path.write_text(content, 'utf-8')
+            if absolute_links:
+                link_path = ''.join((link_prepend, str(parent_notebook.media_path), '/', name))
+            else:
+                link_path = '/'.join((link_prepend, media_dir_name, name))
 
+            if convert_spaces_in_links:
+                link_path = link_path.replace(' ', '%20')
+
+            try:
+                Path(parent_notebook.media_path / name).write_bytes(nsx_file.read('file_' + md5))
+                attachment_list.append('[{}]({})'.format(name, link_path))
+            except Exception:
+                if source:
+                    attachment_list.append('[{}]({})'.format(name, source))
+                else:
+                    print('  Can\'t find attachment "{}" of note "{}"'.format(name, note_title))
+                    attachment_list.append('[NOT FOUND]({})'.format(link_path))
+
+            if ref and source:
+                content = content.replace(ref, source)
+            elif ref:
+                content = content.replace(ref, link_path)
+
+
+        if note_data.get('tag', '') or attachment_list or insert_title \
+                or insert_ctime or insert_mtime:
+            content = '\n' + content
+
+        if insert_mtime and note_mtime:
+            text_mtime = time.strftime('%Y-%m-%d %H:%M', time.localtime(note_mtime))
+            content = 'Modified: {}  \n{}'.format(text_mtime, content)
+        if insert_ctime and note_ctime:
+            text_ctime = time.strftime('%Y-%m-%d %H:%M', time.localtime(note_ctime))
+            content = 'Created: {}  \n{}'.format(text_ctime, content)
+        if attachment_list:
+            content = 'Attachments: {}  \n{}'.format(', '.join(attachment_list), content)
+        if note_data.get('tag', ''):
+            content = 'Tags: {}  \n{}'.format(', '.join(note_data['tag']), content)
+        if insert_title:
+            content = '{}\n{}\n{}'.format(note_title, '=' * len(note_title), content)
+
+        if creation_date_in_filename and note_ctime:
+            note_title = time.strftime('%Y-%m-%d ', time.localtime(note_ctime)) + note_title
+
+        md_file_name = sanitise_path_string(note_title) or 'Unnamed'
+        md_file_path = Path(parent_notebook.path / '{}.{}'.format(md_file_name, md_file_ext))
+
+        n = 1
+        while md_file_path.is_file():
+            md_file_path = Path(parent_notebook.path / ('{}_{}.{}'.format(
+                                            sanitise_path_string(note_title), n, md_file_ext)))
+            n += 1
+
+        md_file_path.write_text(content, 'utf-8')
+        converted_note_count += 1
+
+    for notebook in notebook_id_to_path_index.values():
         try:
-            notebook_media_path.rmdir()
+            notebook.media_path.rmdir()
         except OSError:
             pass
+
 
 pandoc_input_file.close()
 pandoc_output_file.close()
 os.unlink(pandoc_input_file.name)
 os.unlink(pandoc_output_file.name)
 
-input('Conversion finished. Press Enter to quit...')
+input('Converted {} notebooks and {} out of {} notes. Press Enter to quit...'.format(
+            len(config_data['notebook']), converted_note_count, len(config_data['note'])))
